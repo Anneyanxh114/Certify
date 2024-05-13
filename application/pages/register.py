f8d9c1dab243a8754e282ced52eed4f7a94ab6bb
Gong Yiqi <78319531+chloeyiqigong@users.noreply.github.com>
2024-05-13 15:47:28 +0800

    Update register.py

--- application/pages/register.py
+++ application/pages/register.py
@@ -4,9 +4,9 @@ from streamlit_extras.switch_page_button import switch_page
 from utils.streamlit_utils import hide_icons, hide_sidebar, remove_whitespaces
 
 st.set_page_config(layout="wide", initial_sidebar_state="collapsed")
-hide_icons()
-hide_sidebar()
-remove_whitespaces()
+hide_icons()  # Function to hide Streamlit icons
+hide_sidebar()  # Function to hide Streamlit sidebar
+remove_whitespaces()  # Function to remove unnecessary whitespaces
 
 form = st.form("login")
 email = form.text_input("Enter your email")
@@ -14,16 +14,18 @@ password = form.text_input("Enter your password", type="password")
 clicked_login = st.button("Already registered? Click here to login!")
 
 if clicked_login:
-    switch_page("login")
+    switch_page("login") # Switch to the login page
     
 submit = form.form_submit_button("Register")
 if submit:
+    # Call the register function with the provided email and password
     result = register(email, password)
     if result == "success":
+        # Display a success message for successful registration
         st.success("Registration successful!")
         if st.session_state.profile == "Institute":
-            switch_page("institute")
+            switch_page("institute") # Switch to the institute page
         else:
-            switch_page("verifier")
+            switch_page("verifier") # Switch to the verifier page
     else:
-        st.error("Registration unsuccessful!")
+        st.error("Registration unsuccessful!") # Display an error message for unsuccessful registration
