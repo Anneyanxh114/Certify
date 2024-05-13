fcd58dc26f58175f25531be87a80a5bc60bee0a2
Gong Yiqi <78319531+chloeyiqigong@users.noreply.github.com>
2024-05-13 15:08:31 +0800

    Update Migrations.sol

--- contracts/Migrations.sol
+++ contracts/Migrations.sol
@@ -1,24 +1,24 @@
-// SPDX-License-Identifier: MIT 
 pragma solidity ^0.8.13;
 
 contract Migrations {
-  address public owner;
-  uint public last_completed_migration;
+  address public owner; //Address of the contract owner 
+  uint public last_completed_migration; // Stores the last completed migration step
+
 
   modifier restricted() {
-    if (msg.sender == owner) _;
+    if (msg.sender == owner) _; // Only the contract owner can execute the function
   }
 
   constructor() {
-    owner = msg.sender;
+    owner = msg.sender; // Set the contract creator as the owner
   }
 
   function setCompleted(uint completed) public restricted {
-    last_completed_migration = completed;
+    last_completed_migration = completed; // Set the completed migration step
   }
 
   function upgrade(address new_address) public restricted {
-    Migrations upgraded = Migrations(new_address);
-    upgraded.setCompleted(last_completed_migration);
+    Migrations upgraded = Migrations(new_address); // Create a new instance of the Migrations contract at the specified address
+    upgraded.setCompleted(last_completed_migration); // Call the setCompleted function of the upgraded contract
   }
 }
